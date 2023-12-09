db725d243f0a4f289daef5f1987c3365d18c5579
Josh <adesanyajoshua@ymail.com>
2023-12-09 11:50:35 +0100

    graph

--- src/eco.ts
+++ src/eco.ts
@@ -13,25 +13,17 @@ import {
   wasteUpdated as wasteUpdatedEvent
 } from "../generated/eco/eco"
 import {
-  actionCreated,
-  actionUpdated,
-  treeCreated,
-  treeUpdated,
-  userCreated,
-  userUpdatedActions,
-  userUpdatedOverScore,
-  userUpdatedScore,
-  userUpdatedTree,
-  userUpdatedWaste,
-  wasteCreated,
-  wasteUpdated
+ Waste,
+ Tree,
+ Action,
+ User
 } from "../generated/schema"
 
+
+
+
 export function handleactionCreated(event: actionCreatedEvent): void {
-  let entity = new actionCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+  let entity = new Action(event.params.id.toString())
   entity.action_type = event.params.action_type
   entity.description = event.params.description
   entity.proof = event.params.proof
@@ -39,68 +31,65 @@ export function handleactionCreated(event: actionCreatedEvent): void {
   entity.confirmed = event.params.confirmed
   entity.creator = event.params.creator
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
+ 
   entity.save()
 }
 
 export function handleactionUpdated(event: actionUpdatedEvent): void {
-  let entity = new actionUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+  let id = event.params.id.toString()
+  let entity = Action.load(id);
+
+  if (entity == null){
+    entity = new Action(id);
+  }
+ 
+
   entity.status = event.params.status
   entity.confirmed = event.params.confirmed
   entity.creator = event.params.creator
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+ 
 
   entity.save()
 }
 
+
+
 export function handletreeCreated(event: treeCreatedEvent): void {
-  let entity = new treeCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+
+  let entity = new Tree(event.params.id.toString())
   entity.no_of_trees = event.params.no_of_trees
   entity.locations = event.params.locations
   entity.status = event.params.status
   entity.confirmed = event.params.confirmed
   entity.creator = event.params.creator
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
   entity.save()
 }
 
 export function handletreeUpdated(event: treeUpdatedEvent): void {
-  let entity = new treeUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+
+  let id = event.params.id.toString()
+  let entity = Tree.load(id);
+
+  if (entity == null){
+    entity = new Tree(id);
+  }
+ 
   entity.status = event.params.status
   entity.confirmed = event.params.confirmed
   entity.creator = event.params.creator
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+ 
 
   entity.save()
 }
 
 export function handleuserCreated(event: userCreatedEvent): void {
-  let entity = new userCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+
+  let entity = new User(event.params.id.toString())
+ 
+
   entity.trees = event.params.trees
   entity.waste = event.params.waste
   entity.actions = event.params.actions
@@ -108,25 +97,94 @@ export function handleuserCreated(event: userCreatedEvent): void {
   entity.score = event.params.score
   entity.user = event.params.user
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
   entity.save()
 }
 
-export function handleuserUpdatedActions(event: userUpdatedActionsEvent): void {
-  let entity = new userUpdatedActions(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+ export function handleuserUpdatedActions(event: userUpdatedActionsEvent): void {
+
+  
+  let entity = User.load(event.params.id.toString());
+
+  if (entity == null){
+    entity = new User(event.params.id.toString());
+  }
+
+  
   entity.actions = event.params.actions
   entity.overall_score = event.params.overall_score
   entity.score = event.params.score
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.save()
+}
+
+
+
+export function handleuserUpdatedTree(event: userUpdatedTreeEvent): void {
+  let entity = User.load(event.params.id.toString());
+
+  if (entity == null){
+    entity = new User(event.params.id.toString());
+  }
+
+  entity.trees = event.params.trees
+  entity.overall_score = event.params.overall_score
+  entity.score = event.params.score
+
+
+  entity.save()
+}
+
+export function handleuserUpdatedWaste(event: userUpdatedWasteEvent): void {
+  let entity = User.load(event.params.id.toString());
+
+  if (entity == null){
+    entity = new User(event.params.id.toString());
+  }
+
+  entity.waste = event.params.waste
+  entity.overall_score = event.params.overall_score
+  entity.score = event.params.score
+
+
+  entity.save()
+} 
+
+export function handlewasteCreated(event: wasteCreatedEvent): void {
+  let entity = new Waste(event.params.id.toString())
+
+ 
+  entity.weight = event.params.weight
+  entity.sorted = event.params.sorted
+  entity.status = event.params.status
+  entity.confirmed = event.params.confirmed
+  entity.creator = event.params.creator
+
+  entity.save()
+}
+
+export function handlewasteUpdated(event: wasteUpdatedEvent): void {
+  let id = event.params.id.toString()
+  let entity = Waste.load(id);
+
+  if (entity == null){
+    entity = new Waste(id);
+  }
+
+  entity.status = event.params.status
+  entity.confirmed = event.params.confirmed
+  entity.creator = event.params.creator
+
+  entity.save()
+}
+
+export function handleuserUpdatedScore(event: userUpdatedScoreEvent): void {
+  let entity = User.load(event.params.id.toString());
+
+  if (entity == null){
+    entity = new User(event.params.id.toString());
+  }
+
+  entity.score = event.params.score
 
   entity.save()
 }
@@ -134,95 +192,13 @@ export function handleuserUpdatedActions(event: userUpdatedActionsEvent): void {
 export function handleuserUpdatedOverScore(
   event: userUpdatedOverScoreEvent
 ): void {
-  let entity = new userUpdatedOverScore(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
+  let entity = User.load(event.params.id.toString());
+
+  if (entity == null){
+    entity = new User(event.params.id.toString());
+  }
+ 
   entity.overall_score = event.params.overall_score
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleuserUpdatedScore(event: userUpdatedScoreEvent): void {
-  let entity = new userUpdatedScore(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
-  entity.score = event.params.score
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleuserUpdatedTree(event: userUpdatedTreeEvent): void {
-  let entity = new userUpdatedTree(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
-  entity.trees = event.params.trees
-  entity.overall_score = event.params.overall_score
-  entity.score = event.params.score
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleuserUpdatedWaste(event: userUpdatedWasteEvent): void {
-  let entity = new userUpdatedWaste(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
-  entity.waste = event.params.waste
-  entity.overall_score = event.params.overall_score
-  entity.score = event.params.score
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlewasteCreated(event: wasteCreatedEvent): void {
-  let entity = new wasteCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
-  entity.weight = event.params.weight
-  entity.sorted = event.params.sorted
-  entity.status = event.params.status
-  entity.confirmed = event.params.confirmed
-  entity.creator = event.params.creator
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlewasteUpdated(event: wasteUpdatedEvent): void {
-  let entity = new wasteUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.eco_id = event.params.id
-  entity.status = event.params.status
-  entity.confirmed = event.params.confirmed
-  entity.creator = event.params.creator
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
